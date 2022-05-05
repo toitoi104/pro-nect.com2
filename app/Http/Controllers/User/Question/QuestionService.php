43280dd5a94b61b90d4b1539c83d57e74090223d
shimizu toshinori <>
2022-05-06 02:37:03 +0900

    codeの表示CSSを変更

--- app/Http/Controllers/User/Question/QuestionService.php
+++ app/Http/Controllers/User/Question/QuestionService.php
@@ -32,16 +32,26 @@ class QuestionService
 
     public function create(User $user, QuestionRequest $request): void
     {
+        $contents = $this->changeContents($request->getContents());
+
         $question = new Question();
 
         $question->setUserId($user->getId());
-        $question->setContents($request->getContents());
+        $question->setContents($contents);
         $question->setType($request->getType());
         $question->setTitle($request->getTitle());
         $question->setPublic(true);
         $question->setSolved(false);
 
         $this->questionRepository->add($question);
+    }
+
+    private function changeContents(string $contents): string
+    {
+        $contents = preg_replace("/</", "&lt;", $contents);
+        $contents = preg_replace('/\>/', '&gt;', $contents);
+
+        return $contents;
     }
 
     public function answer(User $user, AnswerRequest $request): void
--- resources/views/common/showContent.blade.php
+++ resources/views/common/showContent.blade.php
@@ -1,15 +1,19 @@
+<script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?skin=sunburst"></script>
+
 <div>
-    @foreach($questionAnswer->getContentsRows() as $row)
-        @if(preg_match("/\<code\>/", $row))
-            <div class="text-white bg-gray-700 p-3">
-                <code>
-        @elseif(preg_match("/\<\/code\>/", $row))
-                </code>
-            </div>
-        @elseif(empty($row))
-            <br>
-        @else
-            {!! nl2br(e($row)) !!}
-        @endif
-    @endforeach
+@foreach($questionAnswer->getContentsRows() as $row)
+    @if(preg_match("/\[code\]/", $row))
+<div class="">
+    <code>
+<pre class="prettyprint p-3">
+    @elseif(preg_match("/\[\/code\]/", $row))
+</pre>
+    </code>
+</div>
+    @elseif(empty($row))
+<br>
+    @else
+{!! $row !!}
+    @endif
+@endforeach
 </div>
