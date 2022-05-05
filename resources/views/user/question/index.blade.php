0ecc8ea1e13c02ee194c7c727b545ceff27710bf
shimizu toshinori <>
2022-05-06 02:50:26 +0900

    質問処理の微修正

--- resources/views/user/question/index.blade.php
+++ resources/views/user/question/index.blade.php
@@ -10,7 +10,7 @@
 
         @include('layouts.error.showAllError')
 
-        <form action="{{route('user.question.create')}}" method="post">
+        <form action="{{route('user.question.create')}}" method="post" onsubmit="return false;">
             <div class="flex mb-3">
                 <select class="px-2 py-1 border border-solid border-gray-400 focus:outline-none focus:border-blue-400 hover:border-blue-400 mr-3"
                         name="{{\App\Models\Question\Question::TYPE}}">
@@ -31,7 +31,8 @@
                       class="border border-gray-400 p-1 w-full rounded focus:outline-none focus:border-blue-300">{{old(\App\Models\Question\Question::CONTENTS)}}</textarea>
 
             <div class="flex justify-center">
-                <button type="submit" class="text-xs text-white bg-blue-600 hover:bg-blue-700 rounded-sm px-9 py-3 font-sans">
+                <button type="button" onclick="submit();"
+                        class="text-xs text-white bg-blue-600 hover:bg-blue-700 rounded-sm px-9 py-3 font-sans">
                     質問する
                 </button>
             </div>
